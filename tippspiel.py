7cc1d6294c2b8fbf6fad33b73e92b2c0f64972a9
chasingtomorrow358 <chasingtomorrow358@gmail.com>
2025-08-31 16:01:11 +0200

    Update tippspiel.py

--- tippspiel.py
+++ tippspiel.py
@@ -15,15 +15,36 @@ sheet = client.open_by_key(st.secrets["gspread"]["sheet_id"]).sheet1
 # -------------------------------
 # App-Titel
 # -------------------------------
-st.title("🏆 100m Männer Tippspiel mit Leaderboard")
+st.title("🏆 VFV Spandau: Das große Tippspiel")
 
 # -------------------------------
 # Benutzereingaben
 # -------------------------------
 name = st.text_input("Dein Name")
-a = st.text_input("100m M S:")
-b = st.text_input("100m M Z:")
-c = st.text_input("100m M D:")
+hmme = st.text_input("100m Männer Sieger:")
+hmmz = st.text_input("100m Männer Zweiter:")
+hmmd = st.text_input("100m Männer Dritter:") #100m Männer
+hmwe = st.text_input("100m Frauen Siegerin:")
+hmwz = st.text_input("100m Frauen Zweite:")
+hmwd = st.text_input("100m Frauen Dritte:") #100m Frauen
+hzmhme = st.text_input("110m Hürden Männer Sieger:")
+hzmhmz = st.text_input("100m M Z:")
+hzmhmd = st.text_input("100m M D:") #110m Hürden Männer
+hmhwe = st.text_input("100m Hürde Frauen Siegerin:")
+hmhwz = st.text_input("100m M Z:")
+hmhwd = st.text_input("100m M D:") #100m Hürden Frauen
+zmms= st.text_input("100m M S:")
+zmmz = st.text_input("100m M Z:")
+zmmd = st.text_input("100m M D:") #200m Männer
+zmws = st.text_input("100m M S:")
+zmwz = st.text_input("100m M Z:")
+zmwd = st.text_input("100m M D:") #200m Frauen
+vmms = st.text_input("100m M S:")
+vmmz = st.text_input("100m M Z:")
+vmmd = st.text_input("100m M D:") #400m Männer
+vmwe = st.text_input("100m M S:")
+vmwz = st.text_input("100m M Z:")
+vmwd = st.text_input("100m M D:") #400m Frauen
 
 # -------------------------------
 # Punkteberechnung & Leaderboard-Update
@@ -31,7 +52,8 @@ c = st.text_input("100m M D:")
 if st.button("Auswerten"):
     import sieger  # deine Datei mit den Siegern
 
-    hmm = [a, b, c]
+    hmm = [hmme, hmmz, hmmd]
+    hmw = [hmwe, hmwz, hmwd]
     punkte = 0
 
     # Punkteberechnung
@@ -44,6 +66,48 @@ if st.button("Auswerten"):
     for x in hmm:
         if x in sieger.ohmm:
             punkte += 1
+    #Punkte 100m Frauen
+    if hmw[0] == sieger.ohmw[0]:
+        punkte += 2
+    if hmw[1] == sieger.ohmw[1]:
+        punkte += 2
+    if hmw[2] == sieger.ohmw[2]:
+        punkte += 2
+    for x in hmw:
+        if x in sieger.ohmw:
+            punkte += 1
+    #Punkte 110m Hürden Männer
+        if hmm[0] == sieger.ohmm[0]:
+        punkte += 2
+    if hmm[1] == sieger.ohmm[1]:
+        punkte += 2
+    if hmm[2] == sieger.ohmm[2]:
+        punkte += 2
+    for x in hmm:
+        if x in sieger.ohmm:
+            punkte += 1
+    #100m Hürden Frauen
+        if hmm[0] == sieger.ohmm[0]:
+        punkte += 2
+    if hmm[1] == sieger.ohmm[1]:
+        punkte += 2
+    if hmm[2] == sieger.ohmm[2]:
+        punkte += 2
+    for x in hmm:
+        if x in sieger.ohmm:
+            punkte += 1
+    #100m Hürden Frauen
+        if hmm[0] == sieger.ohmm[0]:
+        punkte += 2
+    if hmm[1] == sieger.ohmm[1]:
+        punkte += 2
+    if hmm[2] == sieger.ohmm[2]:
+        punkte += 2
+    for x in hmm:
+        if x in sieger.ohmm:
+            punkte += 1
+
+    
 
     st.success(f"{name}, du hast {punkte} Punkte!")
 
@@ -79,6 +143,7 @@ if not df.empty and "Punkte" in df.columns:
     st.dataframe(df.sort_values(by="Punkte", ascending=False))
 else:
     st.write("Noch keine Einträge im Leaderboard.")
+
 
 
 
