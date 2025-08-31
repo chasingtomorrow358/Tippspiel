1213f7a72e47673a89239675a2503fc0498e759e
chasingtomorrow358 <chasingtomorrow358@gmail.com>
2025-08-31 16:06:05 +0200

    Update tippspiel.py

--- tippspiel.py
+++ tippspiel.py
@@ -27,25 +27,25 @@ hmmd = st.text_input("100m Männer Dritter:") #100m Männer
 hmwe = st.text_input("100m Frauen Siegerin:")
 hmwz = st.text_input("100m Frauen Zweite:")
 hmwd = st.text_input("100m Frauen Dritte:") #100m Frauen
-hzmhme = st.text_input("110m Hürden Männer Sieger:")
-hzmhmz = st.text_input("100m M Z:")
-hzmhmd = st.text_input("100m M D:") #110m Hürden Männer
-hmhwe = st.text_input("100m Hürde Frauen Siegerin:")
-hmhwz = st.text_input("100m M Z:")
-hmhwd = st.text_input("100m M D:") #100m Hürden Frauen
-zmms= st.text_input("100m M S:")
-zmmz = st.text_input("100m M Z:")
-zmmd = st.text_input("100m M D:") #200m Männer
-zmws = st.text_input("100m M S:")
-zmwz = st.text_input("100m M Z:")
-zmwd = st.text_input("100m M D:") #200m Frauen
-vmms = st.text_input("100m M S:")
-vmmz = st.text_input("100m M Z:")
-vmmd = st.text_input("100m M D:") #400m Männer
-vmwe = st.text_input("100m M S:")
-vmwz = st.text_input("100m M Z:")
-vmwd = st.text_input("100m M D:") #400m Frauen
-
+#hzmhme = st.text_input("110m Hürden Männer Sieger:")
+#hzmhmz = st.text_input("100m M Z:")
+#hzmhmd = st.text_input("100m M D:") #110m Hürden Männer
+#hmhwe = st.text_input("100m Hürde Frauen Siegerin:")
+#hmhwz = st.text_input("100m M Z:")
+#hmhwd = st.text_input("100m M D:") #100m Hürden Frauen
+#zmms= st.text_input("100m M S:")
+#zmmz = st.text_input("100m M Z:")
+#zmmd = st.text_input("100m M D:") #200m Männer
+#zmws = st.text_input("100m M S:")
+#zmwz = st.text_input("100m M Z:")
+#zmwd = st.text_input("100m M D:") #200m Frauen
+#vmms = st.text_input("100m M S:")
+#vmmz = st.text_input("100m M Z:")
+#vmmd = st.text_input("100m M D:") #400m Männer
+#vmwe = st.text_input("100m M S:")
+#vmwz = st.text_input("100m M Z:")
+#vmwd = st.text_input("100m M D:") #400m Frauen
+                                        
 # -------------------------------
 # Punkteberechnung & Leaderboard-Update
 # -------------------------------
@@ -77,35 +77,35 @@ if st.button("Auswerten"):
         if x in sieger.ohmw:
             punkte += 1
     #Punkte 110m Hürden Männer
-        if hmm[0] == sieger.ohmm[0]:
-        punkte += 2
-    if hmm[1] == sieger.ohmm[1]:
-        punkte += 2
-    if hmm[2] == sieger.ohmm[2]:
-        punkte += 2
-    for x in hmm:
-        if x in sieger.ohmm:
-            punkte += 1
+ #       if hmm[0] == sieger.ohmm[0]:
+ #       punkte += 2
+ #   if hmm[1] == sieger.ohmm[1]:
+ #       punkte += 2
+ #   if hmm[2] == sieger.ohmm[2]:
+ #       punkte += 2
+ #   for x in hmm:
+ #       if x in sieger.ohmm:
+ #           punkte += 1
     #100m Hürden Frauen
-        if hmm[0] == sieger.ohmm[0]:
-        punkte += 2
-    if hmm[1] == sieger.ohmm[1]:
-        punkte += 2
-    if hmm[2] == sieger.ohmm[2]:
-        punkte += 2
-    for x in hmm:
-        if x in sieger.ohmm:
-            punkte += 1
+ #       if hmm[0] == sieger.ohmm[0]:
+ #       punkte += 2
+ #   if hmm[1] == sieger.ohmm[1]:
+ #       punkte += 2
+ #   if hmm[2] == sieger.ohmm[2]:
+ #       punkte += 2
+ #   for x in hmm:
+ #       if x in sieger.ohmm:
+ #           punkte += 1
     #100m Hürden Frauen
-        if hmm[0] == sieger.ohmm[0]:
-        punkte += 2
-    if hmm[1] == sieger.ohmm[1]:
-        punkte += 2
-    if hmm[2] == sieger.ohmm[2]:
-        punkte += 2
-    for x in hmm:
-        if x in sieger.ohmm:
-            punkte += 1
+ #       if hmm[0] == sieger.ohmm[0]:
+ #       punkte += 2
+ #   if hmm[1] == sieger.ohmm[1]:
+ #       punkte += 2
+ #   if hmm[2] == sieger.ohmm[2]:
+ #       punkte += 2
+ #   for x in hmm:
+ #      if x in sieger.ohmm:
+ #           punkte += 1
 
     
 
@@ -143,6 +143,7 @@ if not df.empty and "Punkte" in df.columns:
     st.dataframe(df.sort_values(by="Punkte", ascending=False))
 else:
     st.write("Noch keine Einträge im Leaderboard.")
+
 
 
 
