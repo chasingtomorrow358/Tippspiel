fd7ee15f1284d8486098f8178cc655b67a7b067e
chasingtomorrow358 <chasingtomorrow358@gmail.com>
2025-08-31 23:09:16 +0200

    Update sieger.py

--- sieger.py
+++ sieger.py
@@ -1,19 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Aug 26 14:04:09 2025
-
-@author: hoeni_wy8o0xn
-"""
-
-#offhmmeins=print("Sieger 100m M")
-#offhmmzwei=print("zweiter 100m M")
-#offhmmdrei=print("dritter 100m M")
-#offhmmeins="lyles"
-#offhmmzwei="sevile"
-#offhmmdrei="Thompson"
-
 ohmm=["Lyles","Thompson","Seville"]
 ohmw=["Alfred","Clayton","Wood"]
+
 
 
 
